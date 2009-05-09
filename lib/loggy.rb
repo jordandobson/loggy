f02c347b0cc64d85b5ba2917b1ba38916abeb8b3
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-05-08 18:07:30 -0700

    Added test and started YML cache

--- lib/loggy.rb
+++ lib/loggy.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
-require 'resolv'
+require "resolv"
+require "yaml"
 
 class Loggy
   VERSION = '1.0.0'
@@ -9,7 +10,8 @@ class Loggy
   end
   
   def replace_ip line
-  
+    line =~ /^(.+)\s(-\s){2}/
+    line.gsub!($1, get_name($1))
   end  
   
 end
--- test/test_loggy.rb
+++ test/test_loggy.rb
@@ -1,11 +1,14 @@
 require "test/unit"
 require "loggy"
+require "resolv"
+require "yaml"
 
 class TestLoggy < Test::Unit::TestCase
 
   def setup
-    @text_line = '208.77.188.166 - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
-    @loggy = Loggy.new
+    @text_line  = '208.77.188.166 - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
+    @loggy      = Loggy.new
+    @cache      = YAML.load_file('test/test_cache.yml')
   end
   
   def test_ip_returns_correct_name
@@ -15,9 +18,16 @@ class TestLoggy < Test::Unit::TestCase
   end
   
   def test_ip_gets_replaced
-    actual = @loggy.replace_ip @line
-    expected = 'example.com - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
+    actual = @loggy.replace_ip @text_line
+    expected = 'www.example.com - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
     assert_equal expected, actual
   end
+
+  def test_ip_returns_name_from_cache
+    actual   = @loggy.get_name '208.77.188.165'
+    expected = 'www.example.com'
+    assert_equal expected, actual
+  end
+
   
 end
