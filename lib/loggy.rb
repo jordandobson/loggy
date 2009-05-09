d43ed282a5c01aba76b6a89c5e66910ba65b66ad
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-05-08 18:31:17 -0700

    Tested YAML Cache

--- lib/loggy.rb
+++ lib/loggy.rb
@@ -4,14 +4,24 @@ require "yaml"
 
 class Loggy
   VERSION = '1.0.0'
+  CACHE_7_DAYS = 604800
+  
+  def initialize cache_file
+    @cache = cache_file
+  end
   
   def get_name ip
-    Resolv.getname ip
+    if !@cache[ip]
+      Resolv.getname ip
+    else
+      @cache[ip]['name']
+    end
+
   end
   
   def replace_ip line
     line =~ /^(.+)\s(-\s){2}/
     line.gsub!($1, get_name($1))
-  end  
+  end
   
 end
--- test/test_loggy.rb
+++ test/test_loggy.rb
@@ -3,12 +3,17 @@ require "loggy"
 require "resolv"
 require "yaml"
 
+
 class TestLoggy < Test::Unit::TestCase
 
   def setup
     @text_line  = '208.77.188.166 - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
-    @loggy      = Loggy.new
-    @cache      = YAML.load_file('test/test_cache.yml')
+    @cache      = YAML.load_file "test/test_cache.yml"
+    @loggy      = Loggy.new @cache 
+  end
+  
+  def Resolv.getname ip
+    'www.example.com'
   end
   
   def test_ip_returns_correct_name
@@ -25,9 +30,8 @@ class TestLoggy < Test::Unit::TestCase
 
   def test_ip_returns_name_from_cache
     actual   = @loggy.get_name '208.77.188.165'
-    expected = 'www.example.com'
+    expected = '188-165.lax.icann.org'
     assert_equal expected, actual
   end
 
-  
 end
