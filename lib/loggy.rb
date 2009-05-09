e913186976c52f44d88b8167df499aa1764d9370
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-05-08 17:28:36 -0700

    First Tests

--- lib/loggy.rb
+++ lib/loggy.rb
@@ -1,3 +1,15 @@
+require 'test/unit'
+require 'resolv'
+
 class Loggy
   VERSION = '1.0.0'
+  
+  def get_name ip
+    Resolv.getname ip
+  end
+  
+  def replace_ip line
+  
+  end  
+  
 end
--- test/test_loggy.rb
+++ test/test_loggy.rb
@@ -2,7 +2,22 @@ require "test/unit"
 require "loggy"
 
 class TestLoggy < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+
+  def setup
+    @text_line = '208.77.188.166 - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
+    @loggy = Loggy.new
   end
+  
+  def test_ip_returns_correct_name
+    actual   = @loggy.get_name '208.77.188.166'
+    expected = 'www.example.com'
+    assert_equal expected, actual
+  end
+  
+  def test_ip_gets_replaced
+    actual = @loggy.replace_ip @line
+    expected = 'example.com - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
+    assert_equal expected, actual
+  end
+  
 end
